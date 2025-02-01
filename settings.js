c2d176e4ac9cf77609d6cb4f0b2e89df7e394667
Lokuachikingboyoffical <109135065+Lokuachikingboyoffical@users.noreply.github.com>
2025-02-01 16:40:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WiJWEIib#CSjq4F4_vwqBI-SglVUy0oh3lVOV0XZjkUq6z37jYd0' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XqphEY4J#f3EQOX8sBiNaBufLk1BvinapOr7q44cjYoQqkY_Lw1Y' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '9473737373,9363636367' : process.env.SUDO,
